9595526bf32cc9cc30e5da85d3c474ff50ec06da
Do Van Doanh <dovandoanh2k3@gmail.com>
2024-11-16 14:53:27 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -8,16 +8,19 @@ import 'react-toastify/dist/ReactToastify.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import ErrorBoundary from './components/ErrorBoundary/Error';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-    </React.StrictMode>
+      <React.StrictMode>
+          <BrowserRouter>
+            <ErrorBoundary>
+                <App />
+            </ErrorBoundary>
+          </BrowserRouter>
+      </React.StrictMode>
   </Provider>
 );
 
